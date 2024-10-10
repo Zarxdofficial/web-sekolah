f62a5390f503157f21a53c66b9164e7515e882fe
Zarxd development <nizarnozatafa@gmail.com>
2024-10-10 17:42:24 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cp -r ./src/* dist/
 rm ./dist/index.html
 npx html-minifier --collapse-whitespace --remove-comments --remove-redundant-attributes --case-sensitive ./src/index.html -o ./dist/index.html
 rm ./src/css/tailwind.css
-echo '<!--Project: website-mipa-satu | https://github.com/Khuirul-Huda/website-mipa-satu | Khuirul-Huda (@khuirul_huda) -->' > ./dist/index2.html
+echo '<!--Project: website-ipa-sepuluh-satu | https://github.com/Zarxdofficial/web-sekolah | Nizar (@Zarxdoffical) -->' > ./dist/index2.html
 cat ./dist/index.html >> ./dist/index2.html
 rm ./dist/index.html
 mv ./dist/index2.html ./dist/index.html
